871ceb7b93ef1e4146d7bf9dddd0001c79a47014
supriya <supriya.v@indpro.se>
2014-06-09 13:23:48 +0530

    updated on 9/6/2014

--- CopernicusMobile/src/se/copernicus/Android/AndroidEmulator.java
+++ CopernicusMobile/src/se/copernicus/Android/AndroidEmulator.java
@@ -4,20 +4,19 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.net.URL;
 
-import org.apache.commons.collections.map.HashedMap;
-import org.openqa.selenium.internal.seleniumemulation.KeyEvent;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.*;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-public class AndroidSimulator {
+public class AndroidEmulator {
 	RemoteWebDriver driver=null;
 		
 		@BeforeMethod
@@ -26,9 +25,10 @@ public class AndroidSimulator {
 			capabilities.setCapability("appium-version", "1.0");
 			capabilities.setCapability("platformName", "Android");
 			capabilities.setCapability("platformVersion", "4.3");
-			capabilities.setCapability("app", "//CopernicusMobileWorkspace/CTimeSheet_v1.9.6_test.apk");
+			capabilities.setCapability("app", "./CTimeSheet_v2.0.0.apk");
 			capabilities.setCapability("appPackage", "se.copernicus");
 			capabilities.setCapability("appActivity", ".SplashScreenPage");
+			//set the current machine url
 			driver = new RemoteWebDriver(new URL("http://192.168.1.84:4725/wd/hub"), capabilities);
 			driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);     
 		}
@@ -47,33 +47,16 @@ public class AndroidSimulator {
 			List<WebElement> editText=driver.findElements(By.className("android.widget.EditText"));
 			editText.get(0).sendKeys("10");
 			driver.navigate().back();
-			
+	
 			//Require Field Validation method
 			RequiredFieldValidation();
-			
-			editText.get(1).click();
-			editText.get(1).sendKeys("1000");
-			
-			//continue login with incorrect url value
-			IncorrectUrl();
-///			driver.findElement(By.xpath("//android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.widget.TextView[1]")).click();
-//			WebElement pass=driver.findElements(By.className("android.widget.EditText")).get(0);
-//			pass.sendKeys("password");
-//			List<WebElement> ButtonClick=driver.findElements(By.className("android.widget.Button"));
-//			ButtonClick.get(0).click();
-//			ButtonClick.get(0).click();
-//			WebDriverWait wait=new WebDriverWait(driver, 120);
-//			List<WebElement> okButton=driver.findElements(By.className("android.widget.Button"));
-//			wait.until(ExpectedConditions.visibilityOfAllElements(okButton));
-//			List<WebElement> infoRequired=driver.findElements(By.className("android.widget.TextView"));
-//	     	String eMsg=infoRequired.get(1).getText();
-//			Assert.assertEquals("Cannot find any server with the given URL. Check that the URL is correct and that the server is running and then try again", eMsg);
-//			okButton.get(0).click();
-//			driver.findElement(By.className("android.widget.ImageView")).click();
-//     		driver.findElement(By.className("android.widget.ImageView")).click();
-     		
+			editText.get(2).sendKeys("192.168.1.109:7070");
+									
+			//continue login with incorrect data value
+			VerifyIncorrectData();
+			     		
      		//Edit User Details method
-     		//EditUserLoginDetails();
+    		EditUserLoginDetails();
 			driver.findElement(By.xpath("//android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.widget.TextView[1]")).click();
      		editText.get(0).sendKeys("password");
 			List<WebElement> Button=driver.findElements(By.className("android.widget.Button"));
@@ -87,7 +70,7 @@ public class AndroidSimulator {
 			EditTimeReport();
 			
 			//DeleteTimeReport
-			DeleteTimeReport();
+			//DeleteTimeReport();
 			
 			//Logout
 			Logout();
@@ -104,23 +87,35 @@ public class AndroidSimulator {
 			okClick.get(0).click();
 		}
 		
-		public void IncorrectUrl()
+		public void VerifyIncorrectData()
 		{
-			List<WebElement> user=driver.findElements(By.className("android.widget.EditText"));
-			user.get(2).sendKeys("192.168.1.109:7070");
+			List<WebElement> editText=driver.findElements(By.className("android.widget.EditText"));
+			editText.get(1).sendKeys("123");
 			driver.navigate().back();
 			List<WebElement> saveButton=driver.findElements(By.className("android.widget.Button"));
 			saveButton.get(0).click();
+			driver.findElement(By.xpath("//android.widget.RelativeLayout[1]/android.widget.LinearLayout[1]/android.widget.TextView[1]")).click();
+     		editText.get(0).sendKeys("password");
+			List<WebElement> ButtonClick=driver.findElements(By.className("android.widget.Button"));
+			ButtonClick.get(0).click();
+			ButtonClick.get(0).click();
+			WebDriverWait wait=new WebDriverWait(driver, 120);
+			List<WebElement> okButton=driver.findElements(By.className("android.widget.Button"));
+			wait.until(ExpectedConditions.visibilityOfAllElements(okButton));
+			List<WebElement> infoRequired=driver.findElements(By.className("android.widget.TextView"));
+			String eMsg=infoRequired.get(1).getText();
+			Assert.assertEquals("Login failed. Check that the username, company and password is correct and then try again.", eMsg);
+			okButton.get(0).click();
 		}
 		
 		public void EditUserLoginDetails()
 		{
-			//Code to change url and verify warning message
+			//Code to change user detail and verify warning message
+			driver.findElement(By.className("android.widget.ImageView")).click();
+     		driver.findElement(By.className("android.widget.ImageView")).click();
 			List<WebElement> editText=driver.findElements(By.className("android.widget.EditText"));
-			editText.get(2).click();
-			editText.get(2).click();
-			editText.get(2).clear();
-			editText.get(2).sendKeys("192.168.1.109");
+			editText.get(1).clear();
+			editText.get(1).sendKeys("1000");
 			driver.navigate().back();
 			List<WebElement> saveButton=driver.findElements(By.className("android.widget.Button"));
 			saveButton.get(0).click();
@@ -129,15 +124,12 @@ public class AndroidSimulator {
 			Assert.assertEquals("Do you want to modify user detail", eMsg);
      		List<WebElement> okClick=driver.findElements(By.className("android.widget.Button"));
      		okClick.get(0).click();
-     		saveButton.get(0).click();
-		}
+   		}
 		public void AddTimeReport()
 		{
-		List<WebElement> week = driver.findElements(By.className("android.widget.Button"));
-		week.get(24).click();
 		WebDriverWait wait=new WebDriverWait(driver, 240);
 		//click on "+" symbol to add time report
-		List<WebElement> addTimeReport=driver.findElements(By.xpath("//android.widget.RelativeLayout[1]/android.view.View[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.TabHost[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.ImageView[1]"));
+		List<WebElement> addTimeReport=driver.findElements(By.xpath("//android.widget.RelativeLayout[1]/android.view.View[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.TabHost[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.LinearLayout[2]/android.widget.LinearLayout[1]/android.widget.ImageView[1]"));
 		wait.until(ExpectedConditions.visibilityOfAllElements(addTimeReport));
 		addTimeReport.get(0).click();
 		//click on "customer,Project and Activity" text
@@ -166,9 +158,12 @@ public class AndroidSimulator {
 
 		public void EditTimeReport()
 		{
+			//Click on Day tab
+			driver.findElement(By.xpath("//android.widget.RelativeLayout[1]/android.view.View[1]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.LinearLayout[1]/android.widget.TabHost[1]/android.widget.LinearLayout[1]/android.widget.TabWidget[1]/android.widget.RelativeLayout[3]/android.widget.TextView[1]")).click();
+			//Value hard coded
 			driver.findElement(By.name("1090/")).click();
 			List<WebElement> editText=driver.findElements(By.className("android.widget.EditText"));
-			editText.get(0).click();
+			editText.get(0).clear();
 			editText.get(0).sendKeys("Edited comment");
 			driver.navigate().back();
 			//Click on "Hours" button and edit hours
@@ -182,20 +177,21 @@ public class AndroidSimulator {
 			Buttons.get(1).click();
 		}
 		
-		public void DeleteTimeReport()
-		{
-			driver.findElement(By.name("1090/")).click();
-			//driver.getKeyboard().sendKeys(AndroidKeyCode.MENUsdfdsf);
-			//AppiumDriver.sendKeyEvent(AndroidKeyCode.MENU);
-			driver.findElement(By.xpath("//android.view.View[1]/android.widget.TextView[1]")).click();
-			driver.findElement(By.className("android.widget.Button")).click();
-		}
+//		public void DeleteTimeReport()
+//		{
+//			driver.findElement(By.name("1090/")).click();
+//			//trying to get the option menu but the code is not completed
+//			driver.findElement(By.xpath("//android.view.View[1]/android.widget.TextView[1]")).click();
+//			driver.findElement(By.className("android.widget.Button")).click();
+//		}
 		public void Logout()
 		{
 			List<WebElement> logoutLink=driver.findElements(By.className("android.widget.ImageView"));
 			logoutLink.get(0).click();
 			//Click on logout button
-			List<WebElement> textView = driver.findElements(By.xpath("//android.widget.RelativeLayout[1]/android.view.View[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.ListView[1]/android.widget.RelativeLayout[6]/android.widget.TextView[1]"));
+			List<WebElement> textView = driver.findElements(By.xpath("//android.widget.RelativeLayout[1]/android.view.View[2]/android.widget.FrameLayout[1]/android.widget.FrameLayout[1]/android.widget.RelativeLayout[1]/android.widget.ListView[1]/android.widget.RelativeLayout[6]/android.widget.TextView[1]"));                               
+			String txt=textView.get(0).getText();
+			Reporter.log(txt, true);
 			textView.size();
 			textView.get(0).click();
 			List<WebElement> okButton=driver.findElements(By.className("android.widget.Button"));
