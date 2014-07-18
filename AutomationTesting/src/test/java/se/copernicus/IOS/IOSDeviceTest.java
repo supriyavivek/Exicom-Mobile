a425d6c02aff4e4815480a0349a67df2b10aa947
supriya <supriya.v@indpro.se>
2014-07-18 14:16:50 +0530

    App launched from jenkins

--- AutomationTesting/src/test/java/se/copernicus/IOS/IOSDeviceTest.java
+++ AutomationTesting/src/test/java/se/copernicus/IOS/IOSDeviceTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -48,7 +48,7 @@ public class IOSDeviceTest {
         }
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         wd.quit();
     }
